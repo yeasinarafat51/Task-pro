a0ba3d6eba1afa98ed357f08e8fb1aacb472f081
yeasinarafat51 <aloondokar65@gmail.com>
2024-09-04 14:12:19 +0600

    application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Application from "./components/Application";
 import Banner from "./components/Banner";
 import Nabvar from "./components/Nabvar";
 
 function App() {
   return (
     <>
-     <div className="container">
+     <div className="mx-4">
      <Nabvar/>
      <Banner/>
+     <Application/>
      </div>
     </>
   );
